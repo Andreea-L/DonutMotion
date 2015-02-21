57b983bc85836c82fce2af150468d92fa33df270
LausDaeus <niklas.zwingenberger@gmail.com>
2015-02-21 23:27:59 +0000

    Added skiiiiiiboooorrxxx

--- js/test.js
+++ js/test.js
@@ -1,3 +1,5 @@
+var camera, controls, scene, renderer;
+
 if ( ! Detector.webgl ) {
 
 				Detector.addGetWebGLMessage();
@@ -8,8 +10,6 @@ if ( ! Detector.webgl ) {
 			var fogExp2 = true;
 
 			var container, stats;
-
-			var camera, controls, scene, renderer;
 
 			var mesh, mat;
 
@@ -42,7 +42,7 @@ if ( ! Detector.webgl ) {
 				scene.fog = new THREE.FogExp2( 0xffffff, 0.00015 );
 
 				// sides
-				
+				//Makes dem cubes
 				var light = new THREE.Color( 0xffffff );
 				var shadow = new THREE.Color( 0x505050 );
 
@@ -246,6 +246,8 @@ if ( ! Detector.webgl ) {
 
 				window.addEventListener( 'resize', onWindowResize, false );
 
+				makeFancySkyBox();
+
 			}
 
 			function onWindowResize() {
@@ -303,17 +305,36 @@ if ( ! Detector.webgl ) {
 			//
 
 			function animate() {
-
 				requestAnimationFrame( animate );
-
 				render();
-				stats.update();
+				//stats.update();
 
 			}
 
 			function render() {
-
 				controls.update( clock.getDelta() );
 				renderer.render( scene, camera );
+
+			}
+
+			//Makes skybox
+			function makeFancySkyBox(){
+				var axes = new THREE.AxisHelper(100);
+				scene.add(axes);
+				
+				var imagePrefix = "textures/dawnmountain-";
+				var directions  = ["xpos", "xneg", "ypos", "yneg", "zpos", "zneg"];
+				var imageSuffix = ".png";
+				var skyGeometry = new THREE.CubeGeometry( 20000, 20000, 20000);	
+				
+				var materialArray = [];
+				for (var i = 0; i < 6; i++)
+					materialArray.push( new THREE.MeshBasicMaterial({
+						map: THREE.ImageUtils.loadTexture( imagePrefix + directions[i] + imageSuffix ),
+						side: THREE.BackSide
+					}));
+				var skyMaterial = new THREE.MeshFaceMaterial( materialArray );
+				var skyBox = new THREE.Mesh( skyGeometry, skyMaterial );
+				scene.add( skyBox );
 
 			}
