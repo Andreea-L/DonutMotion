09f2e4b3165843559d4a437f351bf56359a02eb4
kstanski <kajetanstanski@gmail.com>
2015-02-22 09:36:57 +0000

    sound yeah!

--- js/test.js
+++ js/test.js
@@ -449,6 +449,7 @@ function checkCollision(){
 		console.log("you exlode");
 		if(plane)
 			makeBoom();
+			stopTheme();
 	}
 }
 
@@ -459,6 +460,16 @@ function makeBoom(){
 	});
 	alive = false
 	scene.remove(plane);
+}
+
+
+var myTheme = new buzz.sound("sounds/FantasyBoss.mp3", {
+	autoplay: true,
+	loop: true
+});
+
+function stopTheme(){
+	myTheme.stop();
 }
 
 function onKeyDown ( event ) {
